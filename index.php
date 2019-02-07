e24f1d82828b3dd504e28420d5f71dc188b03bcd
Phil Plencner <philip_plencner@harvard.edu>
2019-02-06 22:15:47 -0500

    Added homepage with hot dog image

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Hello World</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+
+    <h1>Hello World</h1>
+    <img src='/images/hotdog.jpg' alt='Chicago Style Hot Dog'>
+
+</body>
+</html>
